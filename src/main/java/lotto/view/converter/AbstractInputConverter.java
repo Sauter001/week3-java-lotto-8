e79075bb9c1969e15f108f36eab3170329dc6ff4
sauter001 <lsj457@naver.com>
2025-11-02 00:41:11 +0900

    fix: throw illegal state exception for empty input to pass ApplicationTest

--- src/main/java/lotto/view/converter/AbstractInputConverter.java
+++ src/main/java/lotto/view/converter/AbstractInputConverter.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.NoSuchElementException;
 
 public abstract class AbstractInputConverter<T> implements InputConverter<T> {
+    private static final String INPUT_UNACCEPTABLE = "입력을 더 이상 받을 수 없습니다.";
+
     private final String prompt;
 
     protected AbstractInputConverter(String prompt) {
@@ -14,32 +16,17 @@ public abstract class AbstractInputConverter<T> implements InputConverter<T> {
     @Override
     public final T convert() {
         while (true) {
-            T result = tryParse();
-
-            if (result != null) {
-                return result;
+            try {
+                System.out.println(this.prompt);
+                String input = Console.readLine();
+                return this.parse(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            } catch (NoSuchElementException e) {
+                throw new IllegalStateException(INPUT_UNACCEPTABLE, e);
             }
         }
     }
 
-    private T tryParse() {
-        try {
-            System.out.println(this.prompt);
-            String input = readInputSafely();
-            return this.parse(input);
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-            return null;
-        }
-    }
-
     protected abstract T parse(String input);
-
-    private String readInputSafely() {
-        try {
-            return Console.readLine();
-        } catch (NoSuchElementException e) {
-            return "";
-        }
-    }
 }
